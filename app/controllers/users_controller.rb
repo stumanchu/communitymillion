2e22bc2334d0a28942abc1dd99e5fd8f2efcf02b
CommunityMillion <stuartcarrep@gmail.com>
2018-02-16 05:34:53 +0000

    signup users from ui

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "Welcome to CommunityMillion #{@user.username}"
+        else
+            render 'new'
+        end
+    end
+    
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'donate', to: 'pages#donate'
   get 'faq', to: 'pages#faq'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
