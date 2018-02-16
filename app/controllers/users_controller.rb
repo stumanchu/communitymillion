e1033128c76a1cf073ab4d87dcd6451420c60dc6
CommunityMillion <stuartcarrep@gmail.com>
2018-02-16 06:05:54 +0000

    fix redirect on users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             flash[:success] = "Welcome to CommunityMillion #{@user.username}"
+            redirect_to root_path
         else
             render 'new'
         end
