0f0bef165ab6748e1e16f929e12bf4d1dd727634
CommunityMillion <stuartcarrep@gmail.com>
2018-03-02 04:17:03 +0000

    Add admin user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
     
     def new
         @user = User.new
@@ -31,6 +32,13 @@ class UsersController < ApplicationController
         
     end
     
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:danger] = "User has been deleted"
+        redirect_to root_path
+    end
+    
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
@@ -40,7 +48,7 @@ class UsersController < ApplicationController
     end
     
     def require_same_user
-       if current_user != @user 
+       if current_user != @user and !current_user.admin?
            flash[:danger] = "You can only alter your own account" 
            redirect_to root_path
        end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216042906) do
+ActiveRecord::Schema.define(version: 20180302034940) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180216042906) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
